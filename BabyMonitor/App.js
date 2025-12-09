2a8ab1a1d4c15cad2a161c7ccc205dfc46fd6b1f
amal-alqasim14 <560205@student.fontys.nl>
2025-12-09 08:10:40 +0100

    web versie maken

--- BabyMonitor/App.js
+++ BabyMonitor/App.js
@@ -5,9 +5,6 @@ import AppNavigator from "./navigation/AppNavigator";
 export default function App() {
   return (
     <>
-      {/* <View style={{ padding: 10, backgroundColor: "#eee" }}>
-        <Text style={{ fontSize: 16 }}>Dit staat boven de hele app</Text>
-      </View> */}
       <NavigationContainer>
         <AppNavigator />
       </NavigationContainer>
